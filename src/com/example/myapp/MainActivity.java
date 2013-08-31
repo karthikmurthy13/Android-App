4aa6c6cbdae2d9dd092803a733a37e1cfc29e045
karthikmurthy13 <karthik.murthy13@gmail.com>
2013-08-31 16:28:43 -0400

    Changed Text

--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.myapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		TextView text = (TextView) findViewById(R.id.textView);
+		text.setText("HI MOM");
 		setContentView(R.layout.activity_main);
 	}
 
